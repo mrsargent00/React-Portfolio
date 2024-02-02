103271f2d6ed4db2d3dd344c249a84819c907051
Fluffy <mrsargent00@gmail.com>
2024-02-02 09:14:00 -0500

    created and added code

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css'
+
+import App from './App.jsx'
+import AboutPage from './pages/About';
+import ContactPage from './pages/Contact';
+import PortfolioPage from './pages/Portfiolo.jsx';
+import ResumePage from './pages/Resume';
+
+// define routes
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
